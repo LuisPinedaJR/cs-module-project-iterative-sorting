8356d16a069cfd4bca8bf6c8654d19d3c33603b4
Luis Pineda <lspnd00@gmail.com>
2020-09-04 17:37:16 -0400

    luis-pineda

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,9 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for el in arr:
+        if el == target:
+            return arr.index(el)
+        
     return -1   # not found
 
 
@@ -9,6 +11,23 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
+    #keep track of start and end
+    start = 0
+    end = len(arr)-1
+    
+    #loop through until target is found or end = start
+    while end >= start:
+        #find the middle
+        middle = (start + end) // 2
+        #if the middle is the target return 
+        if arr[middle] == target:
+            return middle
+        #if the target is greater then the middle ignore the left side
+        elif target > arr[middle]:
+            start = middle + 1
+        #if the target is less then the middle ignore the right side
+        elif target < arr[middle]:
+            end = middle - 1
 
     return -1  # not found
+
